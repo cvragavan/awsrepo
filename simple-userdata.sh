33e4d14b7b274fb0ac920325afc2707f6b4fd916
Vijayaragavan C <49911843+ragavancv@users.noreply.github.com>
2019-04-24 23:21:57 +0530

    Create simple-userdata.sh

---
+++ simple-userdata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum update -y
+yum install httpd -y
+service httpd start
+cd /var/www/html
+echo "Hello World" > index.html
+echo "ERROR!!" > error.html
+chkconfig httpd on
+curl http://169.254.169.254/latest/meta-data/local-ipv4 >>index.html
