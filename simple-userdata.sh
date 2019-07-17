16df6dd4dd8ce48f3d60033e4a4bcc2830e1d736
Vijayaragavan C <49911843+cvragavan@users.noreply.github.com>
2019-07-18 00:15:38 +0530

    Update simple-userdata.sh

--- simple-userdata.sh
+++ simple-userdata.sh
@@ -7,3 +7,4 @@ echo "Hello World" > index.html
 echo "ERROR!!" > error.html
 chkconfig httpd on
 curl http://169.254.169.254/latest/meta-data/local-ipv4 >>index.html
+curl http://169.254.169.254/latest/placement/availability-zone >>index.html
