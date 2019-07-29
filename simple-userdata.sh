8b57af6a1048519b77c58c9c5c2d1c7938491a4e
Vijayaragavan C <49911843+cvragavan@users.noreply.github.com>
2019-07-29 20:57:42 +0530

    Update simple-userdata.sh

--- simple-userdata.sh
+++ simple-userdata.sh
@@ -8,3 +8,14 @@ echo "ERROR!!" > error.html
 chkconfig httpd on
 curl http://169.254.169.254/latest/meta-data/local-ipv4 >>index.html
 curl http://169.254.169.254/latest/placement/availability-zone >>index.html
+_____________________________________________________________________________________
+
+#!/bin/bash
+yum update -y
+yum install httpd -y
+service httpd start
+mkdir /var/www/html/orders
+echo "<h1>This is Order</h1>" > /var/www/html/orders/index.html
+curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/orders/index.html
+curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/orders/index.html
+
